28b7ae61453980960261cb9746b271b6023bc664
danelian <141317269+danelian@users.noreply.github.com>
2023-10-17 01:44:09 +0300

    block eventsbl is ready

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,23 +1,41 @@
 // ------------------------- BURGER MENU ------------------------
 const navMenu = document.getElementById('nav-menu'),
-    navToggle = document.getElementById('nav-toggle'),
-    navClose = document.getElementById('nav-close')
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close')
 if (navToggle) {
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show-menu')
-        document.querySelector('body').classList.add('dis-scroll')
-    })
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu')
+    document.querySelector('body').classList.add('dis-scroll')
+  })
 }
 if (navClose) {
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu')
-        document.querySelector('body').classList.remove('dis-scroll')
-    })
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu')
+    document.querySelector('body').classList.remove('dis-scroll')
+  })
 }
 const navLink = document.querySelectorAll('.nav__list li a')
 const linkAction = () => {
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show-menu')
-    document.querySelector('body').classList.remove('dis-scroll')
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show-menu')
+  document.querySelector('body').classList.remove('dis-scroll')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+// ------------------------- SWIPER ------------------------
+var swiper = new Swiper(".eventsSwiper", {
+  slidesPerView: 3,
+  grid: {
+    rows: 2,
+  },
+	navigation: {
+		nextEl: ".eventsbl .swiper-button-next",
+		prevEl: ".eventsbl .swiper-button-prev",
+	},
+  pagination: {
+    el: ".eventsbl .swiper-pagination",
+		type: "fraction",
+    clickable: true,
+  },
+});
